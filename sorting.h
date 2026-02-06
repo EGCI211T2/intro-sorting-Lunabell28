aecb5f501fb21649a36e23f1a1124542c4f3a419
Lunabell28 <klayrung48@gmail.com>
2026-02-07 01:01:02 +0700

    Refactor swap function and improve display output

--- sorting.h
+++ sorting.h
@@ -1,18 +1,8 @@
-#ifndef SORTING_H
-#define SORTING_H
-
 #include <iostream>
 #include <iomanip>
-
 using namespace std;
 
-
-void swap_numbers(int &a, int &b);
-void display(int *a, int N);
-void sort(int *a, int N);
-
-
-void swap_numbers(int &a, int &b) {
+void swap(int &a, int &b) {
     int temp = a;
     a = b;
     b = temp;
@@ -20,6 +10,7 @@ void swap_numbers(int &a, int &b) {
 
 void display(int *a, int N) {
     for (int i = 0; i < N; i++) {
+        // Now 'cout' and 'setw' will be recognized!
         cout << setw(5) << a[i];
     }
     cout << endl;
@@ -28,15 +19,12 @@ void display(int *a, int N) {
 void sort(int *a, int N) {
     int i, k;
     for (i = 1; i < N; i++) {
-       
         for (k = i - 1; k >= 0; k--) {
             if (a[k] > a[k + 1]) {
-                swap_numbers(a[k], a[k + 1]);
+                swap(a[k], a[k + 1]);
             } else {
                 break;
             }
         }
     }
 }
-
-#endif
