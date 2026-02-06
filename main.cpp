114865ec08ac332beb139ae455e87254f25f1f2a
Lunabell28 <klayrung48@gmail.com>
2026-02-07 00:58:19 +0700

    Refactor argument parsing and sorting logic

--- main.cpp
+++ main.cpp
@@ -1,24 +1,53 @@
 #include <iostream>
-#include <cstdlib>   
-#include "sorting.h" 
+#include <cstdlib>
+#include <cctype> /
+#include "sorting.h"
 
 using namespace std;
 
 int main(int argc, char * argv[]) {
-    int i, N;
+    int i;
     int *a;
+    int N = 0; 
+    for (i = 1; i < argc; i++) {
+        bool isNumber = true;
+        int startCheck = 0;
+        if (argv[i][0] == '-') startCheck = 1;
+        for (int j = startCheck; argv[i][j] != '\0'; j++) {
+            if (!isdigit(argv[i][j])) {
+                isNumber = false;
+                break;
+            }
+        }
+    
+        if (isNumber) {
+            N++;
+        }
+    }
 
-    N = argc - 1;
-
-    if (N <= 0) {
+    if (N == 0) {
         cout << "No numbers to sort." << endl;
         return 0;
     }
 
     a = new int[N];
+    int current_index = 0;
+    for (i = 1; i < argc; i++) {
+        bool isNumber = true;
+        int startCheck = 0;
+        if (argv[i][0] == '-') startCheck = 1;
+        
+        for (int j = startCheck; argv[i][j] != '\0'; j++) {
+            if (!isdigit(argv[i][j])) {
+                isNumber = false;
+                break;
+            }
+        }
 
-    for(i = 0; i < N; i++){
-        a[i] = atoi(argv[i + 1]);
+        if (isNumber) {
+            a[current_index] = atoi(argv[i]);
+            current_index++;
+        }
     }
 
     cout << "Before sorting: ";
