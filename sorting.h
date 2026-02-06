64cfeb4256b3b3ea9b322af9f93300bbdbac4ddf
Lunabell28 <klayrung48@gmail.com>
2026-02-06 17:30:45 +0000

    all done

--- sorting.h
+++ sorting.h
@@ -1,3 +1,38 @@
-void swap(int &a,int &b);
-void sort(int *a,int N);
-void display(int *a,int N);
+#include <iostream>  
+#include <iomanip>   
+using namespace std; 
+
+void swap(int &a, int &b);
+void sort(int *a, int N);
+void display(int *a, int N);
+
+
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+
+void display(int *a, int N) {
+    for (int i = 0; i < N; i++) {
+        cout << setw(5) << a[i];
+    }
+    cout << endl;
+}
+
+
+void sort(int *a, int N) {
+    int i, k;
+    for (i = 1; i < N; i++) {
+        
+        for (k = i - 1; k >= 0; k--) {
+            if (a[k] > a[k + 1]) {
+                swap(a[k], a[k + 1]);
+            } else {
+               
+                break;
+            }
+        }
+    }
+}
